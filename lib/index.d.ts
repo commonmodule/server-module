caacaafdbd1245581696484b88e9f9476ec9b3be
yjgaia <yjdev88@gmail.com>
2024-06-19 13:46:23 +0900

    Export WebServerOptions

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,2 +1,3 @@
 export { default as FileServer } from "./webserver/FileServer.js";
+export { WebServerOptions } from "./webserver/WebServer.js";
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,2 @@
 export { default as FileServer } from "./webserver/FileServer.js";
+export { WebServerOptions } from "./webserver/WebServer.js";
