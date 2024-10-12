2fbe472e20d795c6e979040665a985449e8a3ad3
yjgaia <yjdev88@gmail.com>
2024-10-12 13:32:14 +0900

    Update index.d.ts and index.js
    - Update the export statement in index.d.ts and index.js to include the WebServerOptions type.
    - Add content_types.json to the export statement in index.js.
    - Update the package.json version to 0.1.1.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 export { default as FileUtil } from "./util/FileUtil.js";
 export { default as FileServer } from "./webserver/FileServer.js";
 export { default as HttpContext } from "./webserver/HttpContext.js";
-export { WebServerOptions } from "./webserver/WebServer.js";
+export {
+  default as WebServer,
+  WebServerOptions,
+} from "./webserver/WebServer.js";
+export { default as content_types } from "./webserver/content_types.json" assert { type: "json" };
